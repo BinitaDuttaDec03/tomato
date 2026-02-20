db29ca1d242369545e579be1fdc3f6bc9ccd7897
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-02-20 13:15:20 +0530

    Basic express setup for auth service

---
+++ services/auth/dist/index.js
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Auth service is running on port ${PORT}`);
+});
