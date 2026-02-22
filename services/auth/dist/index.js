361d843d7d0f886207b06f7c1de4a151efe14b97
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-02-22 11:53:36 +0530

    Basic login setup

--- services/auth/dist/index.js
+++ services/auth/dist/index.js
@@ -1,8 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import connectDb from "./config/db.js";
-const app = express();
+import authRoutes from "./routes/auth.js";
 const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
     console.log(`Auth service is running on port ${PORT}`);
     connectDb();
--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -1,10 +1,15 @@
 import "dotenv/config";
 import express from "express";
-import connectDb from "./config/db.js";
 
-const app = express();
+import connectDb from "./config/db.js";
+import authRoutes from "./routes/auth.js";
 
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Auth service is running on port ${PORT}`);
