c27c97dd181f77e09afd91476de212313f261d04
BinitaDuttaDec03 <binitadutta777@gmail.com>
2026-02-22 12:14:35 +0530

    Login route - WIP

--- services/auth/dist/controllers/auth.controller.js
+++ services/auth/dist/controllers/auth.controller.js
@@ -1,3 +1,18 @@
-export const loginUser = async (req, res) => {
-    res.json(req.body);
-};
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import tryCatch from "../middlewares/tryCatch.js";
+export const loginUser = tryCatch(async (req, res) => {
+    const { name, email, picture } = req.body;
+    let user = await User.findOne({ email });
+    if (!user) {
+        user = await User.create({
+            name,
+            email,
+            image: picture,
+        });
+    }
+    const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+        expiresIn: "15d",
+    });
+    res.status(200).json({ message: "Login success", token, user });
+});
--- services/auth/src/controllers/auth.controller.ts
+++ services/auth/src/controllers/auth.controller.ts
@@ -1,5 +1,24 @@
-import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
-export const loginUser = async (req: Request, res: Response) => {
-  res.json(req.body);
-};
+import User from "../models/user.model.js";
+import tryCatch from "../middlewares/tryCatch.js";
+
+export const loginUser = tryCatch(async (req, res) => {
+  const { name, email, picture } = req.body;
+
+  let user = await User.findOne({ email });
+
+  if (!user) {
+    user = await User.create({
+      name,
+      email,
+      image: picture,
+    });
+  }
+
+  const token = jwt.sign({ user }, process.env.JWT_SECRET as string, {
+    expiresIn: "15d",
+  });
+
+  res.status(200).json({ message: "Login success", token, user });
+});
